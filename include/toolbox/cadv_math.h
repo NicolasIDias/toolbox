cb6fcf0a94f417a95896f708c6a768786110404f
NicolasIDias <nicolasbhgamer@gmail.com>
2026-04-03 11:36:55 -0300

    feat: add sign function

--- include/toolbox/cadv_math.h
+++ include/toolbox/cadv_math.h
@@ -13,18 +13,29 @@ static inline int MIN_INT(int x, int y) { return ((x < y) ? (x) : (y)); }
     float: MIN_FLOAT)(x, y)
 
 static inline int ABS_INT(int n) { return ((n < 0) ? (-n) : (n)); }
-static inline float ABS_FLOAT(float n) { return ((n < 0) ? (n * -1) : (n)); }
+static inline float ABS_FLOAT(float n) { return ((n < 0.0f) ? (n * -1.0f) : (n)); }
 
 #define ABS(n) _Generic((n), \
     int: ABS_INT,            \
     float: ABS_FLOAT)(n)
 
-static inline int CLAMP_INT(int val, int min, int max) {return ((val < min) ? (min) : (val > max) ? (max) : (val)); }
-static inline float CLAMP_FLOAT(float val, float min, float max) { return ((val < min) ? (min) : (val > max) ? (max) : (val)); }
+static inline int CLAMP_INT(int val, int min, int max) { return ((val < min) ? (min) : (val > max) ? (max)
+                                                                                                   : (val)); }
+static inline float CLAMP_FLOAT(float val, float min, float max) { return ((val < min) ? (min) : (val > max) ? (max)
+                                                                                                             : (val)); }
 
 #define CLAMP(x, y, z) _Generic((x), \
     int: CLAMP_INT,                  \
     float: CLAMP_FLOAT)(x, y, z)
+
+static inline int SIGN_INT(int x) { return ((x < 0) ? -1 : (x >= 1) ? 1
+                                                                    : 0); }
+static inline float SIGN_FLOAT(float x) { return ((x < 0.0f) ? -1.0f : (x >= 1.0f) ? 1.0f
+                                                                                   : 0.0f); }
+
+#define SIGN(x) _Generic((x), \
+    int: SIGN_INT,            \
+    float: SIGN_FLOAT)(x)
 
 #define ROUND(x) ((long long)((x) + 0.5))
 #define EPSILON (1e-9)
