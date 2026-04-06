609f968adcc534fefffeab113491c0f057774ee5
NicolasIDias <nicolasbhgamer@gmail.com>
2026-04-06 07:40:26 -0300

    feat(log): add log debug

--- include/toolbox/cadv_log.h
+++ include/toolbox/cadv_log.h
@@ -15,20 +15,8 @@ static inline void log_info(const char *fmt, ...)
     va_end(args);
 }
 
-static inline void log_debug(const char *fmt, ...)
-{
-    va_list args;
-    va_start(args, fmt);
-    vfprintf(stdout, fmt, args);
-}
 #define LOG_INFO(fmt, ...)          \
     do                              \
     {                               \
         log_info(fmt, __VA_ARGS__); \
     } while (0)
-
-#define LOG_DEBUG(fmt, ...)          \
-    do                               \
-    {                                \
-        log_debug(fmt, __VA_ARGS__); \
-    }
